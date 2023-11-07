18127ab0fad0e0d1da257693e37456689078a0a8
taek2222 <oot309@daum.net>
2023-11-07 20:24:03 +0900

    [Setting] 셋팅 변경

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Button, Alert } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const handlePress = async (action) => {
+    try {
+      const response = await fetch(`http://192.168.137.112:5000/led/${action}`, {
+        method: 'POST', // 또는 'GET', Flask 서버 설정에 맞춰주세요.
+      });
+      const data = await response.json();
+      Alert.alert(data.status);
+    } catch (error) {
+      Alert.alert('Error', 'Cannot communicate with the server');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 50, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Turn ON LED" onPress={() => handlePress('on')} />
+      <Button title="Turn OFF LED" onPress={() => handlePress('off')} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
