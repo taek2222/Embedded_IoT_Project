0f0374a3c5e142cfd97e5b6f1d4af92670acb3b7
taek2222 <oot309@daum.net>
2023-11-18 17:04:57 +0900

    [Project] 온습도 부분 수정

--- Components/contents/Humid.js
+++ Components/contents/Humid.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 function Humid() {
+
+  const [dustData, setDustData] = useState(null);
+
+  // 미세먼지 데이터를 불러오는 함수
+  const fetchDustData = async () => {
+    try {
+      const response = await fetch('http://192.168.137.34:5000/dh11/humid', {
+        method: 'POST',
+      });
+      const data = await response.json();
+      setDustData(data);
+      console.error(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+    // 컴포넌트 마운트 시 및 5초마다 미세먼지 데이터 갱신
+    useEffect(() => {
+      fetchDustData(); // 처음 마운트될 때 데이터 불러오기
+      const interval = setInt
+      
+  
+      // 컴포넌트 언마운트 시 인터벌 정리
+      return () => clearInterval(interval);
+    }, []);
+
   return (
     <View>
       <Text style={styles.humidity_font}>실내 습도</Text>
@@ -12,6 +40,8 @@ function Humid() {
           }}
           style={styles.humidity}
         />
+        <Text style={styles.temperature_value}>{dustData}</Text>
+        <Text style={styles.temperature_value_unit}>°C</Text>
       </View>
     </View>
   );
@@ -39,6 +69,20 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     fontSize: 17,
     fontWeight: "bold",
+  },
+  humidity_value: {
+    width: 45,
+    height: 40,
+    marginTop: 52,
+    marginLeft: 2,
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  humidity_value_unit: {
+    marginTop: 58,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "gray",
   },
 });
 
--- Components/contents/Temperature.js
+++ Components/contents/Temperature.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 function Temperature() {
+
+  const [dustData, setDustData] = useState(null);
+
+  // 미세먼지 데이터를 불러오는 함수
+  const fetchDustData = async () => {
+    try {
+      const response = await fetch('http://192.168.137.34:5000/dh11/temperature', {
+        method: 'POST',
+      });
+      const data = await response.json();
+      setDustData(data);
+      console.error(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  // 컴포넌트 마운트 시 및 5초마다 미세먼지 데이터 갱신
+  useEffect(() => {
+    fetchDustData(); // 처음 마운트될 때 데이터 불러오기
+    const interval = setInterval(fetchDustData, 6000); // 5초마다 반복
+
+    // 컴포넌트 언마운트 시 인터벌 정리
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View>
       <Text style={styles.temperature_font}>실내 온도</Text>
@@ -12,6 +39,8 @@ function Temperature() {
           }}
           style={styles.temperature}
         />
+        <Text style={styles.temperature_value}>{dustData}</Text>
+        <Text style={styles.temperature_value_unit}>°C</Text>
       </View>
     </View>
   );
@@ -39,6 +68,20 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     fontSize: 17,
     fontWeight: "bold",
+  },
+  temperature_value: {
+    width: 45,
+    height: 40,
+    marginTop: 52,
+    marginLeft: 2,
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  temperature_value_unit: {
+    marginTop: 58,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "gray",
   },
 });
 
