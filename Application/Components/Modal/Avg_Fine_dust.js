91a17bd0924ee34bcd0d62418a13074235577527
taek2222 <oot309@daum.net>
2023-11-25 00:04:17 +0900

    [Project] 모달 미세먼지 추가

---
+++ Application/Components/Modal/Avg_Fine_dust.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+function Avg_Fine_dust() {
+  return (
+    <View style={styles.Fine_dust_border}>
+      <Image
+        source={require("../../assets/contents/fine_dust.png")}
+        style={styles.Fine_dust_image}
+      />
+      <Text style={styles.Fine_dust_font}>미세먼지 : </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Fine_dust_border: {
+    // 평균 미세먼지 박스
+    width: 280,
+    height: 80,
+    marginTop: 10,
+    borderWidth: 2,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  Fine_dust_image: {
+    // 평균 미세먼지 이미지
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+  },
+  Fine_dust_font: {
+    // 평균 미세먼지 폰트
+    marginLeft: 5,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default Avg_Fine_dust;
