4c4165713939268172a0a1b92fa3d7ba0aeafead
taek2222 <oot309@daum.net>
2023-12-03 01:30:30 +0900

    [Project] 냉온풍기 텍스트 및 체크박스 추가

--- Application/Components/contents/Hot_cold_fan.js
+++ Application/Components/contents/Hot_cold_fan.js
@@ -1,19 +1,39 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 function Hot_cold_fan() {
+  const [selected, setSelected] = useState('중');
+
+  const Checkbox = ({ label, value }) => (
+    <TouchableOpacity
+      style={styles.checkboxItem}
+      onPress={() => setSelected(value)}>
+      <View style={selected === value ? styles.checkboxChecked : styles.checkboxUnchecked} />
+      <Text>{label}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View>
       <Text style={styles.hc_fan_font}>냉온풍기</Text>
       <View style={styles.hc_fan_border}>
         <Image
-          source={require('../../assets/contents/fan.png')}
+          source={require("../../assets/contents/fan.png")}
           style={styles.hc_fan}
         />
+        <View style={styles.hc_fan_power}>
+          <Text style={styles.hc_fan_power_font}>바람 세기</Text>
+          <View style={styles.checkboxItem}>
+            <Checkbox label="상" value="상" />
+            <Checkbox label="중" value="중" />
+            <Checkbox label="하" value="하" />
+          </View>
+        </View>
       </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   hc_fan_border: {
@@ -37,6 +57,39 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     fontSize: 17,
     fontWeight: "bold",
+  },
+  hc_fan_power: {
+    width: 200,
+    marginTop: 20,
+    alignItems: "center"
+  },
+  hc_fan_power_font: {
+    fontSize: 19,
+    fontWeight: "bold"
+  },
+  checkboxItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginTop: 5,
+  },
+  checkboxUnchecked: {
+    width: 20,
+    height: 20,
+    marginRight: 8,
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: '#000',
+    backgroundColor: '#FFF',
+  },
+  checkboxChecked: {
+    width: 20,
+    height: 20,
+    marginRight: 8,
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: '#000',
+    backgroundColor: '#000',
   },
 });
 
