f07ed81348048e80d622b99e630e131a97878548
taek2222 <oot309@daum.net>
2023-12-03 19:34:50 +0900

    [Project] 문 UI 개선 및 사진 변경

--- Application/Components/contents/Door.js
+++ Application/Components/contents/Door.js
@@ -1,15 +1,39 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 function Door() {
+  const [doorStatus, setDoorStatus] = useState("닫힘"); // 문의 상태를 나타내는 상태 변수
+
+  const getDoorStatusStyle = () => {
+    return {
+      ...styles.door_status,
+      color: doorStatus === "열림" ? "green" : "red",
+    };
+  };
+
   return (
     <View>
       <Text style={styles.door_font}>문</Text>
       <View style={styles.door_border}>
         <Image
-          source={require('../../assets/contents/door.png')}
+          source={require("../../assets/contents/door.png")}
           style={styles.door}
         />
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setDoorStatus("열림")}
+          >
+            <Text style={styles.buttonText}>열기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setDoorStatus("닫힘")}
+          >
+            <Text style={styles.buttonText}>닫기</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={getDoorStatusStyle()}>문 상태: {doorStatus}</Text>
       </View>
     </View>
   );
@@ -23,20 +47,43 @@ const styles = StyleSheet.create({
     marginTop: 5,
     borderWidth: 2,
     borderRadius: 15,
-    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
   },
   door: {
     // 문 사진
-    width: 70,
-    height: 90,
-    marginLeft: 10,
-    marginTop: 25,
+    width: 50,
+    height: 60,
+    marginTop: 5,
   },
   door_font: {
     marginTop: 7,
     marginLeft: 76,
     fontSize: 17,
     fontWeight: "bold",
+  },
+  buttonsContainer: {
+    width: 120,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 10,
+  },
+  button: {
+    borderWidth: 2,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  door_status: {
+    marginTop: 10,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold"
   },
 });
 
