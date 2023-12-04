2e6e181a80db3676ad14ffb80495a9de402d8855
taek2222 <oot309@daum.net>
2023-12-04 20:32:11 +0900

    [Project] Light All 기능 추가

--- Application/Components/contents/Bulb.js
+++ Application/Components/contents/Bulb.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Button, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Image, StyleSheet } from "react-native";
 
 import Bulb_1 from "./Bulb/Bulb_1.js";
 import Bulb_2 from "./Bulb/Bulb_2.js";
@@ -10,7 +10,9 @@ function Bulb() {
 
   const handleAllOn = async () => {
     try {
-      const response = await fetch('http://192.168.137.34:5000/led/all/on', { method: 'POST' });
+      const response = await fetch("http://192.168.137.34:5000/led/all/on", {
+        method: "POST",
+      });
       if (response.ok) {
         setIsAllOn(true);
       }
@@ -18,10 +20,12 @@ function Bulb() {
       console.error("Error turning all LEDs on:", error);
     }
   };
-  
+
   const handleAllOff = async () => {
     try {
-      const response = await fetch('http://192.168.137.34:5000/led/all/off', { method: 'POST' });
+      const response = await fetch("http://192.168.137.34:5000/led/all/off", {
+        method: "POST",
+      });
       if (response.ok) {
         setIsAllOn(false);
       }
@@ -39,8 +43,20 @@ function Bulb() {
         <View style={styles.line} />
         <Bulb_3 isLedOn={isAllOn} />
       </View>
-      <Button title="전체 켜기" onPress={handleAllOn}/>
-      <Button title="전체 끄기" onPress={handleAllOff}/>
+      <View style={{ flexDirection: "row" , marginLeft: 17}}>
+        <TouchableOpacity onPress={handleAllOn}>
+          <Image
+            source={require("../../assets/contents/light_on_all.png")} // '전체 켜기' 버튼 이미지 경로
+            style={styles.buttonImage}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleAllOff}>
+          <Image
+            source={require("../../assets/contents/light_off_all.png")} // '전체 끄기' 버튼 이미지 경로
+            style={styles.buttonImage}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -49,19 +65,25 @@ const styles = StyleSheet.create({
   light_border: {
     // LED 박스
     width: 340,
-    height: 200,
+    height: 220,
     marginTop: 20,
     borderWidth: 2,
     borderRadius: 15,
     flexDirection: "column",
   },
   line: {
-    height: '70%', // 선의 높이
+    height: "70%", // 선의 높이
     marginVertical: 30,
     marginLeft: 17,
     width: 4, // 선의 너비
-    backgroundColor: 'gray', // 선의 색
+    backgroundColor: "gray", // 선의 색
     borderRadius: 100,
+  },
+  buttonImage: {
+    width: 140, // 적절한 크기로 조절
+    height: 50, // 적절한 크기로 조절
+    marginTop: 12,
+    marginHorizontal: 6
   },
 });
 
