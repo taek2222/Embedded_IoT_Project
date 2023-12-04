7f064d45e3b438765baa17f759c49c5998216ebc
taek2222 <oot309@daum.net>
2023-12-05 01:30:15 +0900

    [Project] 내비게이션 추가 전체 앱 수정

--- Application/App.js
+++ Application/App.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Header from './Components/header.js';
-import Contents from './Components/Contents.js';
+import HomeScreen from "./Components/HomeScreen.js";
+import CameraScreen from "./Components/Camera.js";
+import ListScreen from "./Components/ListScreen.js";
 
-function App () {
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={{ marginTop: 50, justifyContent: 'center', alignItems: 'center' }}>
-      <Header/>
-      <Contents/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="메인 화면"
+          component={HomeScreen}
+          options={{
+            tabBarLabelStyle: { fontSize: 14, fontWeight: "bold" },
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require("./assets/bottom_bar/main_screen.png")}
+                style={{ width: size, height: size, tintColor: color }}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="CCTV"
+          component={CameraScreen}
+          options={{ tabBarLabelStyle: { fontSize: 14, fontWeight: "bold" },
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("./assets/bottom_bar/cctv_screen.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ) }}
+        />
+        <Tab.Screen
+          name="출입 기록"
+          component={ListScreen}
+          options={{ tabBarLabelStyle: { fontSize: 14, fontWeight: "bold" },
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("./assets/bottom_bar/list_screen.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ) }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default App;
