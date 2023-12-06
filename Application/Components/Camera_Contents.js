7d64b2bb3d8146335e7ef22b392fe6d70550f406
taek2222 <oot309@daum.net>
2023-12-07 03:06:24 +0900

    [Project] 카메라 리스트 추가 완료

--- Application/Components/Camera_Contents.js
+++ Application/Components/Camera_Contents.js
@@ -1,10 +1,52 @@
-import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
-
 import Header from "./header.js";
 
 function CameraScreen() {
+  const [motionEvents, setMotionEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://172.20.10.2:8080/api/motionevents"
+        );
+        const json = await response.json();
+        setMotionEvents(json);
+      } catch (error) {
+        // 에러 처리
+      }
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const renderItem = ({ item }) => {
+    // 날짜 형식 변경
+    const formattedDate = formatDate(new Date(item.detectionTime));
+
+    return (
+      <View style={style.listItem}>
+        <Text style={style.listItemText}>🚫 감지 시간: {formattedDate}</Text>
+      </View>
+    );
+  };
+
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    const hours = ("0" + date.getHours()).slice(-2);
+    const minutes = ("0" + date.getMinutes()).slice(-2);
+    const seconds = ("0" + date.getSeconds()).slice(-2);
+
+    return `[ ${year} / ${month} / ${day} - ${hours}:${minutes}:${seconds} ]`;
+  };
+
   return (
     <View style={style.Screen}>
       <Header />
@@ -18,7 +60,17 @@ function CameraScreen() {
         source={require("../assets/contents/camera.png")}
         style={style.camera_main}
       />
-      <View style={style.Box_Boder}></View>
+      <View style={style.Box_Boder}>
+        <FlatList
+          data={motionEvents}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
+      <Image
+        source={require("../assets/contents/screenshot.png")}
+        style={style.screenshot}
+      />
     </View>
   );
 }
@@ -39,12 +91,29 @@ const style = StyleSheet.create({
   },
   Box_Boder: {
     width: 340,
-    height: 250,
+    height: 230,
     marginTop: 4,
-    marginBottom: 20,
+    marginBottom: 16,
     borderWidth: 3,
     borderRadius: 30,
     padding: 10,
+  },
+  listItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderBottomWidth: 1.5,
+    borderBottomColor: "gray",
+  },
+  listItemText: {
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  screenshot: {
+    width: 100,
+    height: 80,
+    marginBottom: 30
   },
 });
 
